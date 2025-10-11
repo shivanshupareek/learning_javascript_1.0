32e51968c1e049996e10c74d6b9eb0ac211fc47e
shivanshupareek <shivanshupareek.ps@gmail.com>
2025-10-11 23:25:41 +0530

    data types- Symbols and BigInt

--- src/content/2.primitiveDatatypes.js
+++ src/content/2.primitiveDatatypes.js
@@ -9,7 +9,8 @@ const bigInt = 12939;                           // any number with "n" at the en
 
 const nullValue = null;                            // purposefully writing null as an initial value is called null
 
-const SymbolValue = Symbol("hello");  // a unique value is called a symbol
+const SymbolValue = Symbol("hello");    // a unique value is called a symbol
+const SymbolNewValue = Symbol("hello"); // a unique value is called a symbol
 
 let undefinedValue;                                      // declaring a variable without initialising is called undefined
 
@@ -77,4 +78,53 @@ Symbol
 -----------------
 Symbol is an immutable value which means that once a value has been initialised to Symbol, it can't be changed.
 
-*/
+As you can see in the symbol example above, even though the values of both symbols are the same, the declaration of the
+variable has made it so that they are not the same and their values are individual.
+as another extensive example use of symbol is given below:
+
+let obj = {
+    name: "Shivanshu",
+    age: 25,
+    occupation: "software developer",
+    id: 2851720
+}
+console.log(obj.id)  // this will return the value of the id which is 2851720
+
+let newAge  = Symbol("age")
+obj[newAge] = 25
+console.log(typeof newAge); //newAge is a symbol
+
+now as per the example shown above, even thought we have two key value pairs with the name of "age", they are both
+completely different as one is a symbol which makes itself completely unique and irrespective of any other variable or key
+with the same name which allows us to reuse multiple names without any collision or confusion between them.
+
+
+BigInt
+----------------
+When a number becomes too big in Javascript, it is not able to handle it accurately and can create errors as that
+number is too big to handle, to check that, we can do two things,
+
+let a =  Number.MAX_SAFE_VALUE //this will show the largest number we can use which can safely control(9007199254740991)
+                                 and we can expect errors or bugs in any value bigger than this as javascript cannot
+                                 handle it.
+
+the biggest number possible in Javascript is as follows
+let b = Number.MAX_VALUE    //this will show you the largest possible value (1.7976931348623157e+308) regardless of
+                              it's safety or controllability.
+
+
+anything we add to the max value will not reflect anything since we are already at the max possible value.
+
+So in order to control the value which are bigger than the max safe value, we can use BigInt which simply means that
+we can add an "n" at the end of the number, and it will work as any regular number does without going out of control
+or creating any unexpected bugs in the code.
+
+if you want to add anything to the bigint value, the addition value must also have an "n" at the end of it to work
+with a bigint, for example:
+
+let a = 123456789n
+
+a + 5n  //
+console.log(a) //this will log the value of 123456794 since we added 5 to it but "n" made it recognisable as a bigint to
+                 the variable "a"
+ */
