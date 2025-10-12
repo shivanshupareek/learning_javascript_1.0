b383bad94e41b3303e3a37738070a5522d1df144
shivanshupareek <shivanshupareek.ps@gmail.com>
2025-10-12 16:48:18 +0530

    type coercion

--- src/content/3.referenceDataTypes.js
+++ src/content/3.referenceDataTypes.js
@@ -43,4 +43,47 @@ this is a javascript feature which most developers do not like since most prefer
 over the data type and in huge projects, it can lead to bugs as we could mistakenly reassign any other data type where
 we are not supposed to and that could lead to bugs and unexpected behaviours which is why we have typescript.
 
+----------------------
+typeof quirks
+
+1. typeof null               // it is an object
+2. typeof NaN(Not a Number) // it is a number
+3. NaN === NaN              // false
+3. 1 + "1" = "11"           // it becomes a string (concatenation)
+4. [] + []  = ""            // adding two arrays will give you an empty string
+5.  NaN == NaM              // true
+6.  [] + {}                 // it will return an array with [object Object]
+
+and there are many more like this in javascript but mostly few of them are worth remembering as they influence real
+life coding issues
+
+------------------------
+
+type coercion
+(== vs ====)
+truthy vs falsy values
+
+
+the arithmetic value of add (+) has two features:
+1. it can add
+2. it can concatenate
+
+when there a string, the "+" sign will always. by default, choose to concatenate rather than add, and this is because
+of type coercion.  it can mix two different types.
+to concatenate, javascript will monitor for two things and if both these are fulfilled, it will choose concatenate, and
+the two categories are:
+1. it has to be a string
+2. it needs to have addition.
+
+if both the aforementioned conditions are met, javascript will command the plus sign to concatenate rather than add.
+foe example:
+
+"5" + 5 =  "55"         //this is because it fulfills both condition of sting and add and hence it will join both 5 into
+                          a string
+
+but since this feature is only available with the add sign, it doesn't work with other others,
+for example
+
+"5" - 2 = 3             // this is now a number even though there is a string, since the subtract sign does not have the
+                           ability to concatenate, it will turn the string into a number and then subtract.
 */
