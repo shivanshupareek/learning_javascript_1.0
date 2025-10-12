e60b6fb4ee95c395c3161ec80eff2bc248e95ec9
shivanshupareek <shivanshupareek.ps@gmail.com>
2025-10-12 14:18:27 +0530

    started reference data types

---
+++ src/content/3.referenceDataTypes.js
@@ -0,0 +1,46 @@
+/*
+
+Any variable which has brackets after the assignment types, is a reference value type.
+
+If you reassign a new value to any reference data type, it will reference it to the original variable and the change made
+in the reassigned statement will also be impacted in the original statement since they are both referencing to the same
+original memory.
+
+for example:
+let a = {
+    name: "Shivanshu"
+}
+
+let b = a
+
+b = "Pareek" // so the value of "a" and "b" have both been changed to "Pareek" from "Shivanshu" since they are both
+                referencing to the same "name" key value pairs
+----------------------------
+
+Dynamic typing
+
+Javascript allows dynamic typing which means that, unlike other languages which use static typing, javascript will allow
+to type dynamically which means that the value of a variable can be changed dynamically.
+
+As studied before, there are 7 data types, in static typing, once we declare and initialise a variable with a data type,
+the data type cannot be changed even if it is reassigned, for example:
+
+in static typing, if:
+
+let a = 12 //this is a number, if we reassign another value to "a" it has to be a number, it cannot be any other data type
+a = true //this is a boolean, this is not allowed in a language which does not have dynamic typing.
+
+but in dynamic typing, the value of a variable which is reassigned can be any data type, for example:
+
+let x = 10             // "x" has been initialised with a number
+x = "Shivanshu"        //it has now been reassigned with a string
+x = true              //it has now been reassigned with a boolean
+x = null              //it has now been reassigned with null
+x = Symbol("hi")      //it has now been reassigned with a string
+etc...
+
+this is a javascript feature which most developers do not like since most prefer static typing as it allows control
+over the data type and in huge projects, it can lead to bugs as we could mistakenly reassign any other data type where
+we are not supposed to and that could lead to bugs and unexpected behaviours which is why we have typescript.
+
+*/
